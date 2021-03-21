b1abcee3c7b161db18c5f45b8e14c9bc8c93ec65
Ivan Villarroel Ramirez <ivillram@everis.com>
2021-03-20 22:26:32 -0300

    feature(base project): Added base springboot project and dependencies

---
+++ src/main/java/com/ivillarroelr/TIHI14evdiag/Tihi14EvDiagApplication.java
@@ -0,0 +1,13 @@
+package com.ivillarroelr.TIHI14evdiag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tihi14EvDiagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tihi14EvDiagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivillarroelr/TIHI14evdiag/Tihi14EvDiagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivillarroelr.TIHI14evdiag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tihi14EvDiagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
