affe08e999fcf353c9d1224dcfc0da7d12b77953
Ivan Villarroel Ramirez <ivillram@everis.com>
2021-03-22 19:33:26 -0300

    refactor(model): Added functionality to prevent loop when using models to responseObject

--- src/main/java/com/ivillarroelr/TIHI14evdiag/model/Currency.java
+++ src/main/java/com/ivillarroelr/TIHI14evdiag/model/Currency.java
@@ -1,25 +1,36 @@
 package com.ivillarroelr.TIHI14evdiag.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import net.minidev.json.annotate.JsonIgnore;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Data
+@Table(name = "currency")
+@EqualsAndHashCode(exclude = {"items"})
 public class Currency {
     @Id
-    private int ID;
-
-    @Column(length = 100, nullable = false)
-    private String longname;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int currency_id;
 
     @Column(length = 3, nullable = false)
     private String shortname;
 
-    @OneToMany(mappedBy="currency_id")
+    @Column(length = 100, nullable = false)
+    private String longname;
+
+    @Column(length = 10)
+    private String symbol;
+
+    @JsonBackReference
+    @OneToMany(fetch = FetchType.LAZY, mappedBy="currency_id")
+    @Fetch(value= FetchMode.SELECT)
     private Set<Item> items;
 }
